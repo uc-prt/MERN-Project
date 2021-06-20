17e929668614e9b6cecaaa5b337f46cdf7ab81df
Priya-Tiwari1211 <85149051+Priya-Tiwari1211@users.noreply.github.com>
2021-06-20 23:49:56 +0530

    Update ShowBCAres.js

--- CRUD_MERN/frontend/src/Component/ShowBCAres.js
+++ CRUD_MERN/frontend/src/Component/ShowBCAres.js
@@ -1,23 +1,22 @@
-// import React,{useState} from 'react'
-// import axios from 'axios';
-// // import Showdatarecord from './Showdatarecord';
+import React from 'react'
+import axios from 'axios';
+// import Showdatarecord from './Showdatarecord';
 
 
-// const ShowBCAres = (props) => {
-//     const [results, setResult] = useState([]);
-  
+const ShowBCAres = (props) => {
    
-//     const id=props.id;
-//         axios.get(`http://localhost:7000/studentdb/showResult/${id}`).then((res)=>{
-//             setResult(res.data);
-//         })
+    const id=props.id;
+       const data= axios.get(`http://localhost:7000/studentdb/showResult/${id}`).then((res)=>{
+           document.write("<table><tr><th>Name</th>","<td>"+res.data.Name+"</td></tr></table>");
+        })
+       
+    return (
+        <div className="p-5 m-5">
+          {/* {results} */}
+          
+        </div>
+    )
+}
 
-//     return (
-//         <div className="p-5 m-5">
-//           {results}
-//         </div>
-//     )
-// }
 
-
-// export default ShowBCAres
+export default ShowBCAres
